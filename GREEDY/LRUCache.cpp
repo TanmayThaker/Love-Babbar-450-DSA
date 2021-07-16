0dcda4960c48b79e64919e7ea6b5246d8e9ab62d
Tanmay Thaker <tanmaythaker09@gmail.com>
2021-07-16 18:02:18 +0530

    LRU Cache updated code

--- GREEDY/LRUCache.cpp
+++ GREEDY/LRUCache.cpp
@@ -78,3 +78,84 @@ int main()
 
 	return 0; 
 } 
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//ANOTHER GOOD APPROACH BY STRIVER:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class LRUCache
+{
+  public:
+  class node
+  {
+    int key;
+    int val;
+    node* next;
+    node* prev;
+    node(int _key,int _val)
+    {
+        key=_key;
+        val=_val;
+    }
+  };
+  node* head=new node(-1,-1);
+  node* tail=new node(-1,-1);
+  
+  int cap;
+  unordered_map<int,node*> m;
+  
+  LRUCache(int capacity)
+  {
+      cap=capacity;
+      head->next=tail;
+      tail->prev=head;
+  }
+  void addNode(node* newnode)
+  {
+      node* temp=head->next;
+      newnode->next=temp;
+      newnode->prev=head;
+      head->next=newnode;
+      temp->prev=newnode;
+  }
+  void deleteNode(node* delnode)
+  {
+      node* delprev=delnode->prev;
+      node* delnext=delnode->next;
+      delprev->next=delnext;
+      delnext->prev=delprev;
+  }
+  int get(int key_)
+  {
+      if(m.find(key_)!=m.end())
+      {
+          node* resnode=m[key_];
+          int res=resnode->val;
+          m.erase(key_);
+          deleteNode(resnode);
+          addNode(resnode);
+          m[key_]=head->next;
+          return res;
+      }
+      return -1;
+  }
+  void put(int key_,int value)
+  {
+      if(m.find(key_)!=m.end())
+      {
+          node* existingnode=m[key_];
+          m.erase(key_);
+          deleteNode(existingnode);
+      }
+      if(m.size()==cap)
+      {
+          m.erase(tail->prev->key);
+          deleteNode(tail->prev);
+      }
+      addNode(new node(key_,value));
+      m[key_]=head->next;
+  }
+};
+

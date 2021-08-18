9206adb486d65437451ad45f849f8c892f90dd7c
Tanmay Thaker <tanmaythaker09@gmail.com>
2021-08-18 19:12:46 +0530

    Added new Algorithms

---
+++ Heaps/Convert min Heap to max Heap.cpp
@@ -0,0 +1,58 @@
+//Convert min Heap to max Heap
+#include<bits/stdc++.h>
+using namespace std;
+
+void maxHeapify(int arr[],int i,int n)
+{
+    int l = 2*i+1;
+    int r=2*i+2;
+    int largest = i;
+    if(l<n and arr[l]>arr[i])
+    {
+        largest=l;
+    }
+    if(r<n and arr[r]>arr[i])
+    {
+        largest=r;
+    }
+    if(largest!=i)
+    {
+        swap(arr[i],arr[largest]);
+        maxHeapify(arr,largest,n);
+    }
+}
+
+
+void convertMaxHeap(int arr[],int n)
+{
+    // Start from bottommost and rightmost
+    // internal node and heapify all internal
+    // modes in bottom up way
+    for(int i=(n-2)/2;i>=0;i--)
+    {
+        maxHeapify(arr,i,n);
+    }
+}
+void printArray(int* arr, int size)
+{
+    for (int i = 0; i < size; ++i)
+        printf("%d ", arr[i]);
+}
+
+
+int main()
+{
+    // array representing Min Heap
+    int arr[] = {3, 5, 9, 6, 8, 20, 10, 12, 18, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    printf("Min Heap array : ");
+    printArray(arr, n);
+ 
+    convertMaxHeap(arr, n);
+ 
+    printf("\nMax Heap array : ");
+    printArray(arr, n);
+ 
+    return 0;
+}

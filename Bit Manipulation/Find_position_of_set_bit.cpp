6e182c683ad6303c84e0ec30fb102dcadac748a4
Tanmay Thaker <tanmaythaker09@gmail.com>
2021-08-20 21:10:07 +0530

    Added new algorithms

--- Bit Manipulation/Find_position_of_set_bit.cpp
+++ Bit Manipulation/Find_position_of_set_bit.cpp
@@ -5,40 +5,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
-class Solution {
-  public:
-  bool isPowerOfTwo(int n)
-  {
-      if(n==0) return false;
-      return (n&(n-1))==0;
-      
-  }
-    int findPosition(int N) {
+// } Driver Code Ends
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+        return (n & (n - 1)) == 0;
+    }
+    int findPosition(int N)
+    {
         // code here
-        if(isPowerOfTwo(N)==false) return -1;
-        int count=0;
-        while(N!=0)
+        if (isPowerOfTwo(N) == false)
+            return -1;
+        int count = 0;
+        while (N != 0)
         {
-            N=N>>1;
+            N = N >> 1;
             count++;
-            
         }
         return count;
     }
 };
 
 // { Driver Code Starts.
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         int N;
 
-        cin>>N;
+        cin >> N;
 
         Solution ob;
         cout << ob.findPosition(N) << endl;
     }
     return 0;
-}  // } Driver Code Ends
+} // } Driver Code Ends
